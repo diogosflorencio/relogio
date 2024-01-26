17071c30e33c9ce30bafff7cf411ad9ad93c5594
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-01-26 17:43:10 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,24 @@
+import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs'
+
+const timer = document.getElementById("timer");
+const date = document.getElementById("date");
+
+function exibirHoraAtualizada() {
+    timer.innerText = dayjs().format("HH:mm:ss")
+
+    date.innerText = dayjs().format("dddd")
+}
+
+//loop + sleep 
+
+function aguardar(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function atualizarHoraRegularmente() {
+    while (true) {
+        exibirHoraAtualizada();
+        await aguardar(1000);
+    }
+}
+atualizarHoraRegularmente();
