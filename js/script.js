d705b1c3bae7e3e72eb3a6935f6b288553a2aaf4
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-03-14 21:11:28 -0300

    trancando acesso por link pelo modulo no node

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs'
+// import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs'
+import { dayjs } from 'dayjs';
 
 const timer = document.getElementById("timer");
 const date = document.getElementById("date");
