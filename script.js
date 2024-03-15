6fb6b28695a5c97bd9859d31851aca53ed11641c
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-03-14 21:39:15 -0300

    usando modulo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs'
-import { Dayjs } from 'dayjs';
+// import { Dayjs } from 'dayjs';
+
+var Dayjs = require("dayjs")
 
 const timer = document.getElementById("timer");
 const date = document.getElementById("date");
