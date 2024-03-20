8eb89a4346e0456ad1d9cf0dc4120a658201f2f0
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-03-19 22:26:11 -0300

    refatoração

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs'
-// import { Dayjs } from 'dayjs';
-
 import Dayjs from "dayjs";
 import MicroModal from "micromodal";
-var utc = require('dayjs/plugin/utc')
-var timezone = require('dayjs/plugin/timezone') 
-Dayjs.extend(utc)
-Dayjs.extend(timezone)
+import 'dayjs/plugin/utc';
+import 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
+Dayjs.extend(utc);
+Dayjs.extend(timezone);
 
 // Abrir o modal ao clicar no botão de edição
 document.getElementById('edit-button').addEventListener('click', function() {
@@ -18,26 +17,28 @@ document.getElementById('edit-button').addEventListener('click', function() {
 document.getElementById('save-button').addEventListener('click', function() {
   var selectedTimezone = document.getElementById('timezone-select').value;
   MicroModal.close('timezone-modal');
-  atualizarHoraRegularmente(selectedTimezone)
+  atualizarHoraRegularmente(selectedTimezone);
 });
   
 const timer = document.getElementById("timer");
 const date = document.getElementById("date");
 
-function exibirHoraAtualizada(param) {
-  timer.innerText = Dayjs().tz(param).format("HH:mm:ss")
-  date.innerText = Dayjs().format("dddd")
+async function atualizarHoraRegularmente(param) {
+  while (true) {
+    exibirHoraAtualizada(param);
+    await aguardar(1000);
+  }
 }
-//loop + sleep 
+
+function exibirHoraAtualizada(param) {
+  timer.innerText = Dayjs().tz(param).format("HH:mm:ss");
+  date.innerText = Dayjs().tz(param).format("dddd");
+}
 
 function aguardar(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function atualizarHoraRegularmente(param) {
-    while (true) {
-        exibirHoraAtualizada(param);
-        await aguardar(1000);
-    }
-}
-atualizarHoraRegularmente(param);
+// Obtendo o fuso horário padrão do navegador como parâmetro inicial
+const fusoHorarioPadrao = Intl.DateTimeFormat().resolvedOptions().timeZone;
+atualizarHoraRegularmente(fusoHorarioPadrao);
